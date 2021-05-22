2d03fededb51aa32f6bfc0fd91c489ed6b713d1d
mocakbarmaulana <brindel.development@gmail.com>
2021-05-22 09:34:34 +0700

    layouting admin template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, Xtreme lite admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, Xtreme admin lite design, Xtreme admin lite dashboard bootstrap 5 dashboard template">
+    <meta name="description"
+        content="Xtreme Admin Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Xtreme Admin Lite Template by WrapPixel</title>
+    <link rel="canonical" href="https://www.wrappixel.com/templates/xtreme-admin-lite/" />
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("xtreme-admin/assets/") }}/images/favicon.png">
+    <!-- Custom CSS -->
+    {{-- <link href="{{ asset("xtreme-admin/assets/") }}/libs/chartist/dist/chartist.min.css" rel="stylesheet"> --}}
+    <!-- Custom CSS -->
+    <link href="{{ asset("xtreme-admin/dist/") }}/css/style.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        @include('layouts.module-admin.header')
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        @include('layouts.module-admin.sidebar')
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row align-items-center">
+                    <div class="col-5">
+                        <h4 class="page-title">Dashboard</h4>
+                        <div class="d-flex align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <h1>Ini Container</h1>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <footer class="footer text-center">
+                All Rights Reserved by Xtreme Admin. Designed and Developed by <a
+                    href="https://www.wrappixel.com">WrapPixel</a>.
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{{ asset("xtreme-admin/assets/") }}/libs/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{ asset("xtreme-admin/assets/") }}/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="{{ asset("xtreme-admin/dist/") }}/js/app-style-switcher.js"></script> --}}
+    <!--Wave Effects -->
+    {{-- <script src="{{ asset("xtreme-admin/dist/") }}/js/waves.js"></script> --}}
+    <!--Menu sidebar -->
+    {{-- <script src="{{ asset("xtreme-admin/dist/") }}/js/sidebarmenu.js"></script> --}}
+    <!--Custom JavaScript -->
+    <script src="{{ asset("xtreme-admin/dist/") }}/js/custom.js"></script>
+    <!--This page JavaScript -->
+    <!--chartis chart-->
+    {{-- <script src="{{ asset("xtreme-admin/assets/") }}/libs/chartist/dist/chartist.min.js"></script> --}}
+    {{-- <script src="{{ asset("xtreme-admin/assets/") }}/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js">
+    </script> --}}
+    {{-- <script src="{{ asset("xtreme-admin/dist/") }}/js/pages/dashboards/dashboard1.js"></script> --}}
+</body>
+
+</html>
