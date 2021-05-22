cfadb9db84bfb5df422d3a86d9b487ef18bb79a3
mocakbarmaulana <brindel.development@gmail.com>
2021-05-22 13:39:59 +0700

    tambah field images pada migration product

--- database/migrations/2021_05_22_063436_create_products_table.php
+++ database/migrations/2021_05_22_063436_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('images');
             $table->longText('description');
             $table->unsignedBigInteger('category_id');
             $table->integer('price');
