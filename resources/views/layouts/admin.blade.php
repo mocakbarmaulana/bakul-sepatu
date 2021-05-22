5b3f557d644e967c772742d2d9b8909f05a422e0
mocakbarmaulana <brindel.development@gmail.com>
2021-05-22 11:20:17 +0700

    migration untuk tabel kategori

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,12 +52,12 @@
             <div class="page-breadcrumb">
                 <div class="row align-items-center">
                     <div class="col-5">
-                        <h4 class="page-title">Dashboard</h4>
+                        <h4 class="page-title">{{$active}}</h4>
                         <div class="d-flex align-items-center">
                             <nav aria-label="breadcrumb">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                    <li class="breadcrumb-item"><a>Admin</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">{{$active}}</li>
                                 </ol>
                             </nav>
                         </div>
