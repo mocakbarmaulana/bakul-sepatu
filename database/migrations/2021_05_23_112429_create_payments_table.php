a183c6a90a316013d0b812ccef38c3f4283904da
mocakbarmaulana <brindel.development@gmail.com>
2021-05-23 18:27:06 +0700

    pembuatan migrasi tabel payment

---
+++ database/migrations/2021_05_23_112429_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('order_id');
+            $table->string('name_transfer');
+            $table->string('name_bank');
+            $table->string('number_bank');
+            $table->string('date_transfer');
+            $table->integer('total');
+            $table->string('image_transfer');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
